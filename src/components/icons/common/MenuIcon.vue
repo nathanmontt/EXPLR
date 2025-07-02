63a2d2057e2563ce269523a91dce6d4fe410c4c5
nathanmontt <contato.mont@gmail.com>
2025-07-02 13:33:18 -0300

    Adding content
    Adding a hamburguer menu to a mobile-first code

--- src/components/icons/common/MenuIcon.vue
+++ src/components/icons/common/MenuIcon.vue
@@ -1,10 +1,15 @@
 <template>
     <svg
         xmlns="http://www.w3.org/2000/svg" 
-        height="24px" 
         viewBox="0 -960 960 960" 
-        width="24px" 
         fill="#ECDFCC">
         <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
     </svg>
 </template>
+
+<style scoped>
+    svg { 
+        cursor: pointer;
+        height: 2em;
+    }
+</style>
--- src/components/layout/HeaderComponent.vue
+++ src/components/layout/HeaderComponent.vue
@@ -1,24 +1,43 @@
 <script setup>
-    const homePage = '../../App.vue';
-
-    import BackArrowIcon from '../icons/common/BackArrowIcon.vue';
-    import CloseIcon from '../icons/common/CloseIcon.vue';
+    /* IMPORTS */
     import MenuIcon from '../icons/common/MenuIcon.vue';
-    import MaxVolumeIcon from '../icons/player/MaxVolumeIcon.vue';
-    import MedVolumeIcon from '../icons/player/MedVolumeIcon.vue';
-    import MuteVolumeIcon from '../icons/player/MuteVolumeIcon.vue';
+    import DarkModeIcon from '../icons/modes/DarkModeIcon.vue';
+    import LightModeIcon from '../icons/modes/LightModeIcon.vue';
+    import { ref, reactive } from 'vue';
+    
+    
+    const homePage = reactive({ href:'../../App.vue', id: 'menu' });
+    
+    const menuVisible = ref(false);
+    function toggleMenu () {
+        menuVisible.value = !menuVisible.value
+    }
 </script>
 
 <template>
-    <a :href="homePage">EXPLR</a>
-    <BackArrowIcon/>
-    <MenuIcon/>
-    <MaxVolumeIcon/>
-    <MedVolumeIcon/>
-    <MuteVolumeIcon/>
-    <CloseIcon/>
+    <nav>
+        <div class="options-top">
+            <a :="homePage">EXPLR</a>
+            <MenuIcon v-on:click="toggleMenu" />
+            <ul class="menu-options" v-bind:class="{
+                visible: menuVisible
+            }">
+                <li class="options"><a href="#">ABOUT</a></li>
+                <li class="options"><a href="#">CONTACT</a></li>
+                <li class="options">
+                    <span id="icons">
+                        <button class="btn-cta"><DarkModeIcon/></button>
+                        <button class="btn-cta"><LightModeIcon/></button>
+                    </span>
+                </li>
+            </ul>
+        </div>
+
+    </nav>
 </template>
 
 <style lang="scss">
     @use '../../assets/styles/abstracts/variables' as *;
+    @use '../../assets/styles/layout/header' as *;
+    @use '../../assets/styles/base/base' as *;
 </style>
