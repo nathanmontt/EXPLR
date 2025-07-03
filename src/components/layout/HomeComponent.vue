fdc1d0a9795a14ba2b9bbc97c79ee35721ffbaae
nathanmontt <contato.mont@gmail.com>
2025-07-02 23:17:01 -0300

    Content
    Adding, before the logic, the folders to the project

--- src/components/layout/HomeComponent.vue
+++ src/components/layout/HomeComponent.vue
@@ -1,7 +1,13 @@
-<script setup></script>
+<script setup>
+    import FolderIcon from '../icons/player/FolderIcon.vue';
+
+</script>
 
 <template>
-    
+    <div id="container-folders">
+        <FolderIcon/>
+        <FolderIcon/>
+    </div>
 </template>
 
 <style lang="scss">
