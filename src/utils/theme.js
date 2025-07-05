da14909d84075aa60bb7f45c251fbbb8aebd7dc0
nathanmontt <contato.mont@gmail.com>
2025-07-05 15:00:07 -0300

    Contents
    Adding the light/dark mode in the other fonts (a, p, h1, h2, etc)

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,8 +1,8 @@
 export function setTheme(theme) {
-    document.body.setAttribute('id', theme)
-    localStorage.setItem('theme', theme)
+    document.body.setAttribute('id', theme);
+    localStorage.setItem('theme', theme);
 }
 
 export function getSavedTheme() {
-    return localStorage.getItem('theme') || 'dark-mode'
+    return localStorage.getItem('theme') || 'dark-mode';
 }
