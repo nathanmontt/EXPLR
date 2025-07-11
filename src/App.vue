dc9bd4325dc8ddaed63675575d6c98a949e36918
nathanmontt <contato.mont@gmail.com>
2025-07-11 12:42:12 -0300

    Content
    Adding footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
   import ModalComponent from './assets/components/common/ModalComponent.vue';
+  import FooterComponent from './assets/components/layout/FooterComponent.vue';
   import HeaderComponent from './assets/components/layout/HeaderComponent.vue';
   import HomeComponent from './assets/components/layout/HomeComponent.vue'; 
 </script>
@@ -16,5 +17,9 @@
         <ModalComponent />
       </section>
     </main>
+
+    <footer>
+      <FooterComponent />
+    </footer>
   </body>
 </template>
--- src/assets/components/layout/FooterComponent.vue
+++ src/assets/components/layout/FooterComponent.vue
@@ -5,6 +5,13 @@
 </script>
 
 <template>
+    <div class="container-socials">
+        <ul>
+            <li><a href="#">INS</a></li>
+            <li><a href="#">LIN</a></li>
+            <li><a href="#">GIT</a></li>
+        </ul>
+    </div>
 </template>
 
 <style lang="scss">
@@ -12,4 +19,5 @@
     @use '../../../assets/styles/layout/header' as *;
     @use '../../../assets/styles/base/base' as *;
     @use '../../../assets/styles/theme/themes' as *;
+    @use '../../../assets/styles/layout/footer' as *;
 </style>
